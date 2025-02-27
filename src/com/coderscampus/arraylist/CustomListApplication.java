45372ce418d08a4102a274d7df30a8cb9795610f
TMGan <gangadeentim@gmail.com>
2025-02-26 19:37:32 -0500

    resubmission

--- src/com/coderscampus/arraylist/CustomListApplication.java
+++ src/com/coderscampus/arraylist/CustomListApplication.java
@@ -5,22 +5,13 @@ package com.coderscampus.arraylist;
 public class CustomListApplication {
 	public static void main(String[] args) {
 		CustomList<String> list = new CustomArrayList<>();
-		 for(int i=1; i<=40; i++) {
+		 for(int i=1; i<=80; i++) {
 			 list.add("element" + i);
-
-
 		 }
-		
-		
 
-		
-		
-		
-		for(int i=0; i<list.getSize();i++) {
+		 for(int i=0; i<list.getSize();i++) {
 			System.out.println(list.get(i));
 		}
-	
-
+	}
 	}
 
-}
